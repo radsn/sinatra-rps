59fcc464828419e1dda602761ecc2942d16014b4
Rads <45115368+radsn@users.noreply.github.com>
2024-09-22 16:44:53 +0000

    rock paper scissors through server

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Target: https://rps.matchthetarget.com/
 
+1. bin/dev
+2. bin/server
--- app.rb
+++ app.rb
@@ -1,9 +1,54 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" #reloads all code when update code
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock")do
+  moves = ["rock", "paper", "scissors"]
+  @our_move = "rock"
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra)
+end
+
+get("/paper")do
+  moves = ["rock", "paper", "scissors"]
+  @our_move = "paper"
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra)
+end
+
+get("/scissors")do
+  moves = ["rock", "paper", "scissors"]
+  @our_move = "scissors"
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra)
 end
